dafc9386a41056ae97f232d1ed8c5e4c8f066dfb
RichiKennedy <richard.kennedy@hyperisland.se>
2022-08-03 16:18:30 +0200

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-5 gap-y-10 px-32 py-14 bg-gray-900 text-white">
+      <div className=" space-y-4 ">
+        <h5 className=" font-bold">ABOUT</h5>
+
+        <p className=""> How it works</p>
+
+        <p className="">Newsroom</p>
+
+        <p className="">Investors</p>
+      </div>
+      <div className=" space-y-4 ">
+        <h5 className=" font-bold">SUPPORT</h5>
+
+        <p className=""> How it works</p>
+
+        <p className="">Newsroom</p>
+
+        <p className="">Investors</p>
+      </div>
+      <div className=" space-y-4 ">
+        <h5 className=" font-bold">INSPORATION</h5>
+
+        <p className=""> How it works</p>
+
+        <p className="">Newsroom</p>
+
+        <p className="">Investors</p>
+      </div>
+      <div className=" space-y-4 ">
+        <h5 className=" font-bold">COMMUNITY</h5>
+
+        <p className=""> How it works</p>
+
+        <p className="">Newsroom</p>
+
+        <p className="">Investors</p>
+      </div>
+      <div className=" space-y-4 ">
+        <h5 className=" font-bold">CONTACT</h5>
+
+        <p className=""> How it works</p>
+
+        <p className="">Newsroom</p>
+
+        <p className="">Investors</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
